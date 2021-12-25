65c3cff8dddbdd214bc2be12651495ff984b7184
MathieuSoysal <43273304+MathieuSoysal@users.noreply.github.com>
2021-12-25 23:49:36 +0000

    Deploying to javadoc from @ MathieuSoysal/Java-Maven-library-template@b60f3c634ab2b759caf63a6ae7d75e64634bf1e4 🚀

---
+++ member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"io.github.pseudo","c":"App","l":"App()","u":"%3Cinit%3E()"},{"p":"io.github.pseudo","c":"App","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
